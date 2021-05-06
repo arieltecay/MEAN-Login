06f39ad6753ee93c032aa24c9a89ee7fcc67de3a
arieltecay <arieltecay@gmail.com>
2021-05-06 16:15:22 -0300

    connection with back and adding some design

--- frontend/src/app/component/signin/signin.component.ts
+++ frontend/src/app/component/signin/signin.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) {}
+  user = { email: '', password: '' };
+  ngOnInit(): void {}
+  signIn() {
+    this.authService.signIn(this.user).subscribe(
+      (res) => {
+        console.log(res.token);
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/profile']);
+      },
+      (err) => console.log(err.error)
+    );
   }
-
 }
